f897dd1fdec80fb9c3c261b20928a54ef6649aa9
f4j4rmaulana <f4j4rmaulana@gmail.com>
2024-02-03 18:04:19 +0700

    Router Refactor To Class

--- public/index.php
+++ public/index.php
@@ -2,10 +2,14 @@
 require '../helpers.php'; // fungsi untuk path didalamnya
 // loadView('home'); 
 
+
+require basePath('Router.php');
+
+$router = new Router();
+$routes = require basePath('routes.php');
+
 $uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
 
-// inspectAndDie($uri); check uri
-
-require basePath('router.php');
-
+$router->route($uri, $method);
 ?>
--- router.php
+++ router.php
@@ -1,10 +1,90 @@
 <?php
-$routes = require basePath('routes.php');
 
-if(array_key_exists($uri,$routes)) {
-  require(basePath($routes[$uri])); 
-} else {
-  http_response_code(404); //mengubah kode status di inspect network dari 200 ok menjadi 404
-  require basePath(($routes['404']));
+class Router {
+  protected $routes = [];
+
+  public function registerRoute($method, $uri, $controller) {
+    // akses variable $routes diatas
+    $this->routes[] = [
+      'method' => $method,
+      'uri' => $uri,
+      'controller' => $controller
+    ];
+  }
+
+  /**
+   * Add a GET route
+   * 
+   * @param string $uri
+   * @param string $controller
+   * @return void
+   */
+  public function get($uri, $controller) {
+    $this->registerRoute('GET', $uri, $controller);
+  }
+  
+  /**
+   * Add a POST route
+   * 
+   * @param string $uri
+   * @param string $controller
+   * @return void
+   */
+  public function post($uri, $controller) {
+    $this->registerRoute('POST', $uri, $controller);
+    
+  }
+  
+  /**
+   * Add a PUT route
+   * 
+   * @param string $uri
+   * @param string $controller
+   * @return void
+   */
+  public function put($uri, $controller) {
+    $this->registerRoute('PUT', $uri, $controller);
+  }
+  
+  /**
+   * Add a DELETE route
+   * 
+   * @param string $uri
+   * @param string $controller
+   * @return void
+   */
+  public function delete($uri, $controller) {
+    $this->registerRoute('DELETE', $uri, $controller);
+  }  
+
+  /**
+   * Load error page
+   *@param  int $httpCode
+   *  
+   * @return void
+   */
+  public function error($httpCode = 404) {
+    http_response_code($httpCode);
+    loadView("error/{$httpCode}");
+    exit;
+  }
+
+
+  /**
+   * Route the Request
+   * 
+   * @param string $uri
+   * @param string $method
+   * @return void
+   */
+  public function route($uri, $method) {
+    foreach($this->routes as $route) {
+      if($route['uri'] === $uri && $route['method'] === $method) {
+        require basePath($route['controller']);
+        return;
+      }
+    }
+    $this->error();
+  }
 }
 ?>
