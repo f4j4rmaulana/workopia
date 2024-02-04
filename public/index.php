739a0a68be99a465889d65cbaff973a479b494b5
f4j4rmaulana <f4j4rmaulana@gmail.com>
2024-02-04 11:26:19 +0700

    Database Class Connection

--- public/index.php
+++ public/index.php
@@ -1,7 +1,9 @@
 <?php
-require '../helpers.php'; // fungsi untuk path didalamnya
-// loadView('home'); 
+require '../helpers.php';
+require basePath('Database.php');
+$config = require basePath('config/db.php');
 
+$db = new Database($config);
 
 require basePath('Router.php');
 
