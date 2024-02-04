3b5c8342a515b432e69e116e660b334de485f276
f4j4rmaulana <f4j4rmaulana@gmail.com>
2024-02-04 11:56:14 +0700

    Query Method Fetch Listings

--- public/index.php
+++ public/index.php
@@ -1,17 +1,18 @@
 <?php
 require '../helpers.php';
 require basePath('Database.php');
-$config = require basePath('config/db.php');
-
-$db = new Database($config);
-
 require basePath('Router.php');
 
+// Instatiating the router
 $router = new Router();
+
+// Get routes
 $routes = require basePath('routes.php');
 
+// Get current uri and HTTP method
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 
+// Route the request
 $router->route($uri, $method);
 ?>
