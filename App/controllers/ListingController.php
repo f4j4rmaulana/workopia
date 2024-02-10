0df6b8282031e15bd40b6f890b768292cbff2412
f4j4rmaulana <f4j4rmaulana@gmail.com>
2024-02-10 16:23:20 +0700

    Edit and Update Listing

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -159,6 +159,107 @@ class ListingController {
     } 
       $this->db->queryWithShareLock('DELETE FROM listings WHERE id = :id', $params);
 
+      // Set flash message
+      $_SESSION['success_message'] = 'Listing deleted successfully';
+
       redirect('/listings');
+  }
+
+   /**
+   * Show the listing edit form
+   *
+   * @param array $params
+   * @return void
+   */
+  public function edit($params) 
+  {
+    $id = $params['id'] ?? '';
+
+    $params = [
+      'id' => $id
+    ];
+
+    $listing = $this->db->queryWithShareLock('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+    /**
+     * Error if id listings not found
+     */
+    if (!$listing) {
+      ErrorController::notFound('Listings not found');
+      return;
+      
+    }
+
+      // loadView('error/404');
+      loadView('listings/edit', [
+        'listing' => $listing]);
+  }
+
+  /**
+   * Update a listing
+   * 
+   * @param array $params
+   * @return void
+   */
+  public function update($params) {
+    $id = $params['id'] ?? '';
+
+    $params = [
+      'id' => $id
+    ];
+
+    $listing = $this->db->queryWithShareLock('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+    /**
+     * Error if id listings not found
+     */
+    if (!$listing) {
+      ErrorController::notFound('Listings not found');
+      return;
+    }
+    
+    $allowedFields = ['title', 'description', 'salary', 'requirements', 'benefits', 'company', 'address', 'city', 'province', 'phone', 'email'];
+
+    $valueToUpdate = [];
+
+    $valueToUpdate = array_intersect_key($_POST,array_flip($allowedFields));
+
+    $valueToUpdate = array_map('sanitize', $valueToUpdate);
+
+    $requiredFields = ['title', 'description', 'city', 'province', 'phone', 'email'];
+
+    $errors = [];
+
+    foreach ($requiredFields as $field) {
+      if(empty($valueToUpdate[$field]) || !Validation::string($valueToUpdate[$field])) {
+        $errors[$field] = ucfirst($field) . ' is required';
+      }
+    }
+
+    if(!empty($errors)) {
+      loadView('listings/edit', [
+        'listing'=> $listing,
+        'errors' => $errors
+      ]);
+      exit;
+    } else {
+      // submit to database
+      $updateFields = [];
+
+      foreach(array_keys($valueToUpdate) as $field) {
+        $updateFields[] = "$field = :{$field}";
+      }
+
+      $updateFields = implode(', ', $updateFields);
+
+      $updateQuery = "UPDATE listings SET $updateFields WHERE id = :id";
+
+      $valueToUpdate['id'] = $id;
+      $this->db->queryWithShareLock($updateQuery, $valueToUpdate);
+
+      $_SESSION['success_message'] = 'Listing Updated';
+      
+      redirect('/listings/' . $id);
+    }
   }
 }
